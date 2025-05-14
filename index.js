72cf97ac43887b16651a9456f1f27ae34144098b
joel-frimpong <joel.frimpong25@compscihigh.org>
2025-05-14 17:42:23 +0000

    hhh

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
 
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
+app.use(express.static(__dirname + "/public"))
+
+app.set("view engine", "ejs");
+
+app.use(express.json());
+
+const internshipSchema = new mongoose.Schema({
+
+  title: { type: String },
+  description: { type: String },
+  applicationLink: { type: String },
+  startDate: { type: Number },
+  endDate: { type: Number },
+  testimonials: { type: String },
+  images: { type: String },
+
+ 
+
+})
+
+const Internship = mongoose.model("Internship", countrySchema, "Internships");
+
+app.delete('/title/:title', async (req, res) => {
+    const response = await Internship.findOneAndDelete({ 
+   
+  title: req.params.name
+    })
+   res.json(response)
+})
